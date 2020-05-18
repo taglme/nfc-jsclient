ac44fb82f0645cf6977731e494e5890cbb782a54
vladkampov <vladyslavkampov@gmail.com>
2020-05-18 19:13:01 +0300

    Fix building query params builder for delete endpoints

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,11 +1,11 @@
 export const buildMultipleActionQueryParams = (keyword?: string, ids?: string): string => {
     let queryParams = '';
 
-    if (keyword && !keyword.length) {
+    if (keyword && keyword.length) {
         queryParams += '&keyword=' + keyword;
     }
 
-    if (ids && !ids.length) {
+    if (ids && ids.length) {
         queryParams += '&ids=' + ids;
     }
 
